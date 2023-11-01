c363a2126d9bfeeef7566fb5cb57412600d99d51
JunnieLee <heunghari@naver.com>
2023-11-01 21:14:45 +0900

    10_이중우선순위큐 풀이 2 업로드 - 여전히 실패

--- submission/10_이중우선순위큐.py
+++ submission/10_이중우선순위큐.py
@@ -23,3 +23,34 @@ def solution(operations):
         max_num = -hq.heappop(max_heap) 
         min_num = hq.heappop(min_heap)
         return [max_num, min_num]
+
+# 테스트 케이스 1개 빼고는 다 맞았는데.. 그 하나떄문에 틀림
+# 어떤 케이스를 놓친걸까 ㅇㅅㅇ
+import heapq as hq
+
+def solution(operations):
+    max_heap = []
+    min_heap = []
+    
+    #trash = set()
+    
+    for o in operations:
+        if o[0]=="I":
+            num = int(o[2:])
+            hq.heappush(min_heap,num)
+            hq.heappush(max_heap,-num)
+        elif o[2]=="1":
+            if len(max_heap)>0:
+                hq.heappop(max_heap)
+        else:
+            if len(min_heap)>0:
+                hq.heappop(min_heap)
+    
+    if len(max_heap)*len(min_heap)<1: # 하나라도 길이가 0이면
+        return [0,0]
+    elif (-max_heap[0]==min_heap[0]):
+        return [0,0]
+    else:
+        max_num = -hq.heappop(max_heap) 
+        min_num = hq.heappop(min_heap)
+        return [max_num, min_num]
